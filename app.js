35efeb9ff55923d922cf67d94fb140fedcc3c78e
gtui008 <gtui008@aucklanduni.ac.nz>
2018-09-23 13:34:22 +1200

    Added support for more complex labs to be added to the content area

--- app.js
+++ app.js
@@ -9,74 +9,62 @@ var app = express();
 
 // The app should listen on port 3000, unless a different
 // port is specified in the environment.
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3001);
 
 // Specify that the app should use handlebars, and set handlebars as the app's view engine.
 var handlebars = require("express-handlebars");
 app.engine("handlebars", handlebars({ defaultLayout: "main-layout" }));
 app.set("view engine", "handlebars");
 
-// Specify that when we browse to "/about", render about.handlebars.
-app.get("/html1", function(req, res) {
-  var files = [];
+app.get("/", (req, res) => {
+  // res.render("week6/js4/ex05/animatedbook.html");
 
-  fs.readdirSync(__dirname + "/views/week1/html1/").forEach(file => {
-    // We don't want dot files
-    if (file.charAt(0) != ".") {
-      var j = {
-        title: file
-      };
+  res.render("home");
+});
 
-      files.push(j);
-    }
+app.get("/week6-js4", (req, res) => {
+  var data = {
+    section: "Animated Book"
+  };
+
+  res.render("week6/js4/ex05/animatedbook", {
+    layout: "animatedbook-layout"
   });
-
-  console.log(files);
-
-  var data = {
-    files: files // File names to use for the navbar
-  };
-
-  res.render("week1/html1/ex1/exercise01", data);
 });
 
-// Specify that when we browse to "/contact", render contact.handlebars.
-app.get("/contact", function(req, res) {
-  // Data to send through to the "contact" page
-  var data = {
-    contact: true,
-    name: "Thomas the T-Rex",
-    phoneNumbers: ["+00 0 123 4567", "+64 21 000 0000", "+99 99 999 9999"],
-    email: "thomasTheTRex@notexist.biz",
-    website: "https://www.google.co.nz/"
-  };
+var html1_url = [];
+var html1_files = [];
 
-  // Pass the data as the second argument to send stuff through.
-  res.render("contact", data);
-});
-
-// Specify that when we browse to "/", render home.handlebars
-app.get("/", function(req, res) {
-  fs.readFile(__dirname + "/articles.json", function(err, data) {
-    // Parse the data in the file as JSON.
-    var articles = JSON.parse(data);
-
-    console.log(articles[0]);
-    console.log(articles[1]);
-    console.log(data);
-
-    var data = {
-      home: true,
-      articles: articles
+fs.readdirSync(__dirname + "/views/week1/html1/").forEach(file => {
+  // We don't want dot files
+  if (file.charAt(0) != ".") {
+    html1_url.push(file);
+    var j = {
+      title: file
     };
 
-    // Render home.handlebars, passing in the articles as so.
-    res.render("home", data);
-  });
+    html1_files.push(j);
+  }
 });
+
+console.log(html1_url[0]);
+
+for (let i = 0; i < html1_url.length; i++) {
+  app.get("/html1-" + html1_url[i], (req, res) => {
+    var data = {
+      files: html1_files, // File names to use for the navbar
+
+      section: "HTML1: " + html1_url[i]
+    };
+
+    res.render("week1/html1/" + html1_url[i] + "/" + html1_url[i], data);
+  });
+}
 
 // Allow the server to serve up files from the "public" folder.
 app.use(express.static(__dirname + "/public"));
+
+app.use(express.static(__dirname + "/views"));
 
 // If no other handlers worked, display a 404 page.
 app.use(function(req, res) {
